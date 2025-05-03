aa00969183c3d05ed1964ef52aee315efb9fff79
klaus9267 <klaus9267@gmail.com>
2025-05-03 17:46:09 +0900

    모집글 상세 백업

--- src/component/Router.tsx
+++ src/component/Router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
 import HomePage from "../pages/home/index.tsx";
+import PostDetailPage from "../pages/post/PostDetailPage.tsx";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/post/:id" element={<PostDetailPage />} />
     </Routes>
   );
 }
